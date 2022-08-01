28da972a9db6171b574f80ce2649540d9075cdd6
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-08-01 11:52:16 +0200

    Added generic MappingResult and specialized subclass

---
+++ src/main/java/olif/MappingResult.java
@@ -0,0 +1,29 @@
+package olif;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public abstract class MappingResult {
+
+	protected Path outputPath;
+	
+	public abstract void writeResult();
+	public abstract String getStringResult();
+	
+	
+	/**
+	 * Converts a filePath with a file extension to a path without extension
+	 * @param filePath Path to a file that might contain a file extension (e.g. ".json")
+	 * @return Path without extension
+	 */
+	protected Path removeFileExtension(Path filePath) {
+	    String fileName = filePath.toString();
+		if (fileName == null || fileName.isEmpty()) {
+	        return Paths.get(fileName);
+	    }
+
+	    String extPattern = "(?<!^)[.][^.]*$";
+	    String changedFileName = fileName.replaceAll(extPattern, "");
+	    return Paths.get(changedFileName);
+	}
+}
