2b4379cd4ab928323049f177d9545e19f666b290
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2022-08-01 11:51:56 +0200

    Added abstract mapper with specific subclasses in order to divide code into subclasses

--- src/main/java/olif/json/JsonMapper.java
+++ src/main/java/olif/json/JsonMapper.java
@@ -1,11 +1,14 @@
 package olif.json;
 
+import java.nio.file.Path;
+
+import olif.DataMap;
 import olif.Mapper;
 
 public class JsonMapper extends Mapper {
 
 	@Override
-	public void map() {
+	public void map(DataMap mappingDefinition, Path mappingSourcePath, Path outputFilePath) {
 		// TODO Auto-generated method stub
 		
 	}
